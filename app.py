4a09436a61c11aef619318acf27b39edd171e606
Sam <shivam.saini92@gmail.com>
2022-08-26 16:12:44 -0400

    Eliminate constant refreshing

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ st.title("Demo Dashboard")
 st.write("Demo dashboard created during Queen's Marquee Python class")
 # st.write is similar to print() in Python
 
-categories = ['A', 'B', 'C']
-st.multiselect('Pick an option', categories)
+#categories = ['A', 'B', 'C']
+#st.multiselect('Pick an option', categories)
 
 st.sidebar.button('Click me!')
 
-url = "https://www.iposcoop.com/last-100-ipos/"
-df = pd.read_html(url)[0]
+@st.cache
+def grabData():
+    url = "https://www.iposcoop.com/last-100-ipos/"
+    cachedTable = pd.read_html(url)[0]
+    return cachedTable
 
+df = grabData()
 # st.write(df)
 st.dataframe(df)
 
